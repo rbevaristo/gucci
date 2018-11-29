5672b0ae616992498474d4b9e13a08004547b8fb
Richard Evaristo <richard120189@gmail.com>
2018-11-29 23:03:53 +0800

    fixed bug

--- gucci/js/app.js
+++ gucci/js/app.js
@@ -1,8 +1,8 @@
 let btns = document.getElementsByClassName('btn-buy');
 
 
-for(var i = 0; i < btns.length; i++){
-    btns[i].addEventListener("click", function(){
+for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function() {
         let image = this.parentNode.children[0].src;
         let filename = image.split('/');
         let name = this.parentNode.children[1].innerHTML;
--- gucci/js/shop.js
+++ gucci/js/shop.js
@@ -1,15 +1,15 @@
-function getParam(name){  
-    name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");  
-    var regexS = "[\\?&]"+name+"=([^&#]*)";  
-    var regex = new RegExp( regexS );  
+function getParam(name) {
+    name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+    var regexS = "[\\?&]" + name + "=([^&#]*)";
+    var regex = new RegExp(regexS);
     var results = regex.exec(window.location.href);
-    if(results == null)
-        return "";  
-    else    
+    if (results == null)
+        return "";
+    else
         return results[1];
 }
 
-function getData(){
+function getData() {
     let info = document.getElementById('shop-info');
     info.children[0].src = `./images/${getParam('image')}`;
     info.children[1].innerHTML = getParam('name').replace(/%20/g, ' ');
@@ -19,30 +19,31 @@ function getData(){
 
 let btn = document.getElementById('buy');
 
-btn.addEventListener('click', function(){
+btn.addEventListener('click', function() {
     let inputs = document.getElementsByTagName('input');
     let selectSize = document.getElementById('size');
     let isComplete = true;
-    for(var i = 0; i < inputs.length; i++){
-        if(!inputs[i].value){
-            error(inputs, 'Please fill all fields');
+    for (var i = 0; i < inputs.length; i++) {
+        if (!inputs[i].value) {
             isComplete = false;
         }
     }
-    if(isComplete == true){
+    if (isComplete == true) {
         success(inputs, selectSize);
+    } else {
+        error(inputs, 'Please fill all fields');
     }
 });
 
-function error(inputs, message){
-    for(var i = 0; i < inputs.length; i++){
+function error(inputs, message) {
+    for (var i = 0; i < inputs.length; i++) {
         inputs[i].style = 'border: 1px solid red';
     }
     document.getElementById('shop-output').style = 'display:none';
     alert(message);
 }
 
-function success(inputs, selectSize){
+function success(inputs, selectSize) {
     let labels = document.getElementsByClassName('label');
     let shopOutput = document.getElementById('shop-output');
     let shopList = document.getElementById('shop-info-list');
@@ -50,28 +51,29 @@ function success(inputs, selectSize){
                <p>Product Category: ${getParam('category').replace(/%20/g, ' ')}</p>
                <p>Product Price: ${getParam('price').replace(/%20/g, ' ')}</p>
     `;
-    let change = calculateChange(inputs[inputs.length-1].value);
-
-    for(var i = 0; i < inputs.length; i++){
-        if(i == 1)
+    let change = calculateChange(inputs[inputs.length - 1].value);
+    console.log(change);
+    for (var i = 0; i < inputs.length; i++) {
+        if (i == 1)
             str += `<p>Size: ${selectSize.value}</p>`;
-        if(i == inputs.length-1){
+        if (i == inputs.length - 1) {
             str += `<p>${labels[i].innerHTML} P${inputs[i].value}.00</p>`;
             continue;
         }
         str += `<p>${labels[i].innerHTML} ${inputs[i].value}</p>`;
     }
-    if(!change){
+    if (change < 0) {
         error(inputs, 'Invalid amount!');
+        return false;
     }
     str += `<p>Change: P${change}.00</p>`;
     shopOutput.children[1].children[0].src = `./images/${getParam('image')}`;
     shopList.innerHTML = str;
     shopOutput.style = 'display:block';
-    
+
 }
 
-function calculateChange(amount){
+function calculateChange(amount) {
     let price = parseInt(getParam('price').replace(/%20/g, ' ').split(' ')[1]);
-    return (total = amount - price) > 0 ? total : false; 
+    return (total = amount - price) >= 0 ? total : '';
 }
